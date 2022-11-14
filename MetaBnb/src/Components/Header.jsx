d0fd42a05ae7646ff9098d4a4c5a92c512ba1243
unknown <wyze97@gmail.com>
2022-11-15 00:48:03 +0100

    Style : add style to the header and hero component

---
+++ MetaBnb/src/Components/Header.jsx
@@ -0,0 +1,28 @@
+import homeicon from "../images/Home.png"
+import logo from "../images/logo.png"
+
+export default function Header(){
+
+    return(
+        <section className="Header-section">
+            <div className="header-images">
+                <img src={homeicon} alt="home" />
+                <img src={logo} alt="logo" />
+            </div>
+
+            <div className="header-nav">
+                <ul className="header-nav-elements">
+                    <li>Home</li>
+                    <li>Place to stay</li>
+                    <li>NFTs</li>
+                    <li>Communities</li>
+                </ul>
+            </div>
+
+            <div className="connect-wallet">
+                <button>connect wallet</button>
+            </div>
+        </section>
+    )
+    
+}
---
+++ MetaBnb/src/Components/Hero.jsx
@@ -0,0 +1,36 @@
+import {React, useState} from "react"
+import heroImg from "../images/hero-img.png"
+
+export default function Hero(){
+
+    const [search, setSearch] = useState("")
+
+    function handleChange(event) {
+        setSearch(event.target.value)
+        
+    }
+
+    return(
+        <section className="hero-section">
+            <div className="word-side">
+                <h1 className="hero-header"> Rent a <span className="purple">Place</span> away from <span className="purple">Home</span> in the <span className="purple">Metaverse</span></h1>
+                <p className="hero-paragraph">we provide you access to luxury and affordable houses in the metaverse, get a chance to turn your imagination to reality at your comfort zone</p>
+                <div className="hero-searchbar">
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="Search for Location"
+                            onChange={handleChange}
+                            name = "search"
+                            value={search}
+                        />
+                        <button className="search-button"> Search</button>
+                    </form>
+                </div>
+            </div>
+            <div className="hero-img-side">
+                <img src={heroImg} alt="image" />
+            </div>
+        </section>
+    )
+}
