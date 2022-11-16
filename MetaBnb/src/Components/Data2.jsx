1b281b618df3cad59c6ce2cf56a4ddb650072c2e
unknown <wyze97@gmail.com>
2022-11-16 10:50:19 +0100

    Feat: Add data for place to stay route

---
+++ MetaBnb/src/Components/Data2.jsx
@@ -0,0 +1,163 @@
+export default [
+    {
+        id: 1,
+        name: "Desert King",
+        availability: "2 weeks",
+        coverImg: "Frame1.png",
+        price: "IMBT", 
+        location: "2345",
+        star : "FiveStar.png"
+        
+    },
+    {
+        id: 2,
+        name: "Desert King",
+        availability: "2 weeks",
+        coverImg: "Frame2.png",
+        price: "IMBT", 
+        location: "2345",
+        star : "FiveStar.png"
+        
+    },
+    {
+        id: 3,
+        name: "Desert King",
+        availability: "2 weeks",
+        coverImg: "Frame3.png",
+        price: "IMBT", 
+        location: "2345",
+        star : "FiveStar.png"
+        
+    },
+    {
+        id: 4,
+        name: "Desert King",
+        availability: "2 weeks",
+        coverImg: "Frame4.png",
+        price: "IMBT", 
+        location: "2345",
+        star : "FiveStar.png"
+        
+    },
+    {
+        id: 5,
+        name: "Desert King",
+        availability: "2 weeks",
+        coverImg: "Frame5.png",
+        price: "IMBT", 
+        location: "2345",
+        star : "FiveStar.png"
+        
+    },
+    {
+        id: 6,
+        name: "Desert King",
+        availability: "2 weeks",
+        coverImg: "Frame6.png",
+        price: "IMBT", 
+        location: "2345",
+        star : "FiveStar.png"
+        
+    },
+    {
+        id: 7,
+        name: "Desert King",
+        availability: "2 weeks",
+        coverImg: "Frame7.png",
+        price: "IMBT", 
+        location: "2345",
+        star : "FiveStar.png"
+        
+    },
+    {
+        id: 8,
+        name: "Desert King",
+        availability: "2 weeks",
+        coverImg: "Frame8.png",
+        price: "IMBT", 
+        location: "2345",
+        star : "FiveStar.png"
+        
+    },
+    {
+        id: 9,
+        name: "Desert King",
+        availability: "2 weeks",
+        coverImg: "Frame9.png",
+        price: "IMBT", 
+        location: "2345",
+        star : "FiveStar.png"
+        
+    },
+    {
+        id: 10,
+        name: "Desert King",
+        availability: "2 weeks",
+        coverImg: "Frame10.png",
+        price: "IMBT", 
+        location: "2345",
+        star : "FiveStar.png"
+        
+    },
+    {
+        id: 11,
+        name: "Desert King",
+        availability: "2 weeks",
+        coverImg: "Frame11.png",
+        price: "IMBT", 
+        location: "2345",
+        star : "FiveStar.png"
+        
+    },
+    {
+        id: 12,
+        name: "Desert King",
+        availability: "2 weeks",
+        coverImg: "Frame12.png",
+        price: "IMBT", 
+        location: "2345",
+        star : "FiveStar.png"
+        
+    },
+    {
+        id: 13,
+        name: "Desert King",
+        availability: "2 weeks",
+        coverImg: "Frame13.png",
+        price: "IMBT", 
+        location: "2345",
+        star : "FiveStar.png"
+        
+    },
+    {
+        id: 14,
+        name: "Desert King",
+        availability: "2 weeks",
+        coverImg: "Frame14.png",
+        price: "IMBT", 
+        location: "2345",
+        star : "FiveStar.png"
+        
+    },
+    {
+        id: 15,
+        name: "Desert King",
+        availability: "2 weeks",
+        coverImg: "Frame15.png",
+        price: "IMBT", 
+        location: "2345",
+        star : "FiveStar.png"
+        
+    },
+    {
+        id: 16,
+        name: "Desert King",
+        availability: "2 weeks",
+        coverImg: "Frame16.png",
+        price: "IMBT", 
+        location: "2345",
+        star : "FiveStar.png"
+        
+    },
+    
+];
--- MetaBnb/src/Components/Placetostay.jsx
+++ MetaBnb/src/Components/Placetostay.jsx
@@ -1,9 +1,27 @@
+import Header from "./Header"
+import setting from "../images/setting.svg"
 
 
 export default function Placetostay(){
     return(
         <section>
-            <p>Hi</p>
+            <Header />
+            <div className="place-category">
+                <ul className="category-list">
+                    <li>Resturant</li>
+                    <li>Cottage</li>
+                    <li>Castle</li>
+                    <li>fantast city</li>
+                    <li>beach</li>
+                    <li>Carbins</li>
+                    <li>Off-grid</li>
+                    <li>Farm</li>
+                </ul>
+                <div className="location-setting">
+                    <p>Location</p>
+                    <img src={setting} alt="set" />
+                </div>
+            </div>
         </section>
     )
 }
